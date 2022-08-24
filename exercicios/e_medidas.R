69492bab7c2d21726487fc2132a74d394caec238
rcfeliz <ric.feliz@gmail.com>
2022-08-24 18:45:18 -0300

    aula de tabela de frequência

--- exercicios/e_medidas.R
+++ exercicios/e_medidas.R
@@ -1,6 +1,64 @@
+googlesheets4::gs4_auth("rfeliz@abj.org.br")
+
+# bases para a aula de tabela de frequência --------------------------------------------------
+
+link_tabela_frequencia <- "https://docs.google.com/spreadsheets/d/1-7b4xt0o68sh9fZ-zJ3m16AeoE4mBhHcVnhtmJUUbIg/edit#gid=0"
+
+# base simples
+
+da_simples <- saudeJFCE::da_jfce |>
+  dplyr::sample_n(30) |>
+  dplyr::select(id_processo, assunto) |>
+  dplyr::mutate(
+    ` ` = NA_character_,
+    `  ` = NA_character_,
+    `   ` = NA_character_,
+    `    ` = NA_character_,
+    `     ` = NA_character_,
+    `      ` = NA_character_
+  )
+
+googlesheets4::write_sheet(
+  da_simples,
+  link_tabela_frequencia,
+  "Base simples"
+)
+
+# base grande
+da_grande <- saudeJFCE::da_jfce |>
+  dplyr::select(id_processo, assunto, decisao_sentenca) |>
+  dplyr::mutate(
+    decisao_sentenca = dplyr::case_when(
+      is.na(decisao_sentenca) ~ "ainda não foi decidido",
+      decisao_sentenca == "desfavoravel" ~ "desfavorável",
+      TRUE ~ decisao_sentenca
+    ),
+    assunto = forcats::fct_lump_min(assunto, min = 30, other_level = "Outros")
+  ) |>
+  dplyr::mutate(
+    ` ` = NA_character_,
+    `  ` = NA_character_,
+    `   ` = NA_character_,
+    `    ` = NA_character_,
+    `     ` = NA_character_,
+    `      ` = NA_character_,
+    `       ` = NA_character_,
+    `        ` = NA_character_,
+    `         ` = NA_character_
+
+  )
+
+googlesheets4::write_sheet(
+  da_grande,
+  link_tabela_frequencia,
+  "Base grande"
+)
+
+# bases para os exercícios de medidas ---------------------------------------------------
 link_gabarito <- "https://docs.google.com/spreadsheets/d/17KxDCNXDgE6v1yNiXETyyd1RmetYAMs8LjNYs4J1UXU/edit#gid=0"
 
-link_exercicio <- "a fazer"
+link_exercicio <- "https://docs.google.com/spreadsheets/d/18icIOI2XZX4NR54upLsN0CnLRkyeo6ryQYsTKrZQxL0/edit#gid=0"
+
 
 # Exercício 1 - Tabela de frequência
 
@@ -15,11 +73,9 @@ da_categorica <- abjData::consumo |>
     `      ` = NA_character_
   )
 
-googlesheets4::gs4_auth("rfeliz@abj.org.br")
-
 googlesheets4::write_sheet(
   da_categorica,
-  link_gabarito,
+  link_exercicio,
   "Exercício 1"
 )
 
@@ -36,11 +92,9 @@ da_central <- abjData::pnud_min |>
   dplyr::select(-lat, -lon) |>
   dplyr::filter(ano == 2010)
 
-googlesheets4::gs4_auth("rfeliz@abj.org.br")
-
 googlesheets4::write_sheet(
   da_central,
-  link_gabarito,
+  link_exercicio,
   "Exercício 2"
 )
 
@@ -60,11 +114,9 @@ da_desvio <- abjData::pnud_min |>
     `      ` = NA_character_
   )
 
-googlesheets4::gs4_auth("rfeliz@abj.org.br")
-
 googlesheets4::write_sheet(
   da_desvio,
-  link_gabarito,
+  link_exercicio,
   "Exercício 3"
 )
 
@@ -82,10 +134,13 @@ da_quantil <- readr::read_delim("~/Downloads/ciee.csv", delim = ";") |>
     `      ` = NA_character_
   )
 
-googlesheets4::gs4_auth("rfeliz@abj.org.br")
-
 googlesheets4::write_sheet(
   da_quantil,
-  link_gabarito,
+  link_exercicio,
   "Exercício 4"
 )
+
+
+
+
+
